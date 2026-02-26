11afc0b84e0d2b0b356744868e4d88b2b4645880
Thomas Bolon <tbolon@mb3m.fr>
2026-02-26 16:20:26 +0100

    refacto

--- RtfPipe.Tests/OReillyExamples.cs
+++ RtfPipe.Tests/OReillyExamples.cs
@@ -1,9 +1,4 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace RtfPipe.Tests
